a0121c94b86ea260c772a8835d28e3662ccf3b94
Yella-krishna <krishna@risksek.com>
2024-04-22 15:58:26 +0530

    added sast id

--- hooks/hooks.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -v $(pwd):/src --rm --entrypoint /bin/bash schalla/duckdefender-v2:dev-0.0.69 -c "duckdefender --pre-commit" 
---
+++ hooks/sast.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -v $(pwd):/src --rm --entrypoint /bin/bash schalla/duckdefender-v2:pc-0.0.1 -c "duckdefender --pre-commit sast" 
