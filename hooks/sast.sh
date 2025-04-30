4193ce2c302ec75ac262c32506315062d0ad327b
Yella-krishna <krishna@risksek.com>
2025-04-30 13:07:21 +0530

    test-1

--- hooks/sast.sh
+++ hooks/sast.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
-
-docker run -v $(pwd):/src --rm --entrypoint /bin/bash schalla/duckdefender-v2:pc-0.0.1 -c "duckdefender --pre-commit sast" 
+#!/usr/bin/env bash
+./duckdefender --pre-commit sast
